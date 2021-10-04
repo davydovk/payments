02368b806331cba44099310040174cbf117b46aa
davydovk <davydovk1986@gmail.com>
2021-10-04 15:06:04 +0300

    Добавлена возможность оплаты с помощью QIWI

--- data/config.py
+++ data/config.py
@@ -7,3 +7,9 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+
+
+# Параметры для работы с оплатой QIWI
+QIWI_TOKEN = env.str('QIWI_TOKEN')
+WALLET_QIWI = env.str('WALLET')
+QIWI_PUBKEY = env.str('QIWI_P_PUB')
--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,3 +1,4 @@
 from . import help
 from . import start
+from . import pay_for_item
 from . import echo
